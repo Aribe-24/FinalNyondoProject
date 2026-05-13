0091b00cc430a92eeaa107cf76a2492adba81793
Joan <atimangojoan85@gmail.com>
2026-05-13 12:43:00 +0300

    added the admin dashboard

--- nyondoapp/views.py
+++ nyondoapp/views.py
@@ -16,5 +16,6 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
     return render(request, 'login.html')
-
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
        
